e03e2638ea6a038f69e714b735207c509b0a2814
Jason Swett <jason@benfranklinlabs.com>
2026-04-28 17:04:06 -0400

    Add Numbers.product method.

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -4,4 +4,8 @@ module Numbers
   def self.sum(numbers)
     numbers.sum
   end
+
+  def self.product(numbers)
+    numbers.reduce(1, :*)
+  end
 end
--- test/numbers_test.rb
+++ test/numbers_test.rb
@@ -7,4 +7,8 @@ class NumbersTest < Minitest::Test
   def test_sum_returns_the_sum_of_the_given_numbers
     assert_equal 6, Numbers.sum([1, 2, 3])
   end
+
+  def test_product_returns_the_product_of_the_given_numbers
+    assert_equal 6, Numbers.product([1, 2, 3])
+  end
 end
